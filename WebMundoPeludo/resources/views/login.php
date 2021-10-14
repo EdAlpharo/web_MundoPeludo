44970b0643dca77eb25da00faf2abdc541bcb563
EdAlpharo <alfarovazquezeduardo@gmail.com>
2021-10-14 13:13:14 -0500

    ajuste

--- WebMundoPeludo/resources/views/login.php
+++ WebMundoPeludo/resources/views/login.php
@@ -5,31 +5,31 @@
 <meta charset="UTF-6">
 <title>Login</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-<link rel="stylesheet" href="<?php echo asset('css/base.css')?>" type="text/css">
+<link rel="stylesheet" href="<?php echo asset('css/login.css')?>" type="text/css">
 </head>
 
 <body>
 <h1 class="display-3  text-center" style="color:#BD9B37"; >Inicio de sesion</h1>
 
 <div class="container">
-    
+
   <form  class="contenedorform mt-5">
 
         <div class="form-group">
-        
+
             <label style="color:#DEC57B">Usuario:</label>
            <input type="text" name="txtem" class="form-control">
-        
+
         </div>
           <div class="form-group">
-        
+
             <label style="color:#DEC57B">Contrasena:</label>
             <input type="password" name="txtcont" class="form-control">
-        
+
           </div>
 
-   
-    <button type="submit" name="btnadm" class="btn btn-lg btn-block" style=background-color:#C7BE9E >Entrar</button> 
+
+    <button type="submit" name="btnadm" class="btn btn-lg btn-block" style=background-color:#C7BE9E >Entrar</button>
 
 
 </form>
