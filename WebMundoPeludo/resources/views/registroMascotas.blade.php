e125ca826005bce57b0847f5e3804f15348140c9
EdAlpharo <alfarovazquezeduardo@gmail.com>
2021-10-17 22:00:47 -0500

    ajuste AgregarMascota (Luis)

--- WebMundoPeludo/resources/views/registroMascotas.blade.php
+++ WebMundoPeludo/resources/views/registroMascotas.blade.php
@@ -1,74 +1,72 @@
-<!DOCTYPE html>
-<html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/mascotas.css">
+
+@extends('base')
+
+@section('contenido')
+
+ <link rel="stylesheet" href="css/mascotas.css">
 
     <title>Registro de Mascotas</title>
   </head>
   <body>
-    <br>
-    <h1 class="display-4 text-center" style = "font-family:Calisto MT;"><FONT COLOR="Black">Agregar mascota</FONT></h1>
+    <div class="container-fluid" style="padding: 50px">
 
-    <div id="general">
+        <h1 class="display-4 text-center" style = "font-family:Calisto MT;"><FONT COLOR="Black">Agregar mascota</FONT></h1>
+
+    <div id="general" style="padding: 25px">
        <br>
       <H3 align="center"  style = "font-family:Calisto MT;"><FONT COLOR="black">Formulario de mascotas</FONT></H3>
       <center>
       <img src="img/logo1.png" width="60" height="60" /></center>
 
       <form class="contenedorform2 mt-4">
-        <div class="form-group">
-              <center>
+        <div class="form-group" style="padding: 5px">
+              <left>
            <label><FONT COLOR="white">Especie: </FONT>  </label>
-           <input type="" class="" id="txtEspecie" name="txtEspecie" placeholder="Ingrese la especie del animal" style="WIDTH: 207px;">
-              </center>
+           <input type="" class="" id="txtEspecie" name="txtEspecie" placeholder="Ingrese la especie del animal" style="WIDTH: auto;">
+              </ñ>
         </div>
 
-        <div class="form-group">
-              <center>
+        <div class="form-group" style="padding: 5px">
+              <left>
            <label><FONT COLOR="white">Raza: </FONT>  </label>
-           <input type="" class="" id="txtRaza" name="txtRaza" placeholder="Ingrese la raza del animal" style="WIDTH: 185px;">
-              </center>
+           <input type="" class="" id="txtRaza" name="txtRaza" placeholder="Ingrese la raza del animal" style="WIDTH: auto;">
+              </left>
         </div>
 
-        <div class="form-group">
-              <center>
+        <div class="form-group" style="padding: 5px">
+              <left>
            <label><FONT COLOR="white">Edad: </FONT>  </label>
-           <input type="" class="" id="txtEdad" name="txtEdad" placeholder="Ingrese la edad del animal" style="WIDTH: 190px;">
-              </center>
+           <input type="" class="" id="txtEdad" name="txtEdad" placeholder="Ingrese la edad del animal" style="WIDTH: auto;">
+              </left>
         </div>
 
-        <div class="form-group">
-              <center>
+        <div class="form-group" style="padding: 10px">
+              <left>
            <label><FONT COLOR="white">Condición de salud: </FONT>  </label>
-           <input type="" class="" id="txtCondicion" name="txtCondicion" placeholder="Ingrese si existe alguna condición del animal" style="WIDTH: 240px;">
-              </center>
+           <input type="" class="" id="txtCondicion" name="txtCondicion" placeholder="Ingrese si existe alguna condición del animal" style="WIDTH: auto;">
+              </left>
         </div>
 
-        <div class="form-group">
-              <center>
+        <div class="form-group" style="padding: 10px">
+              <left>
            <label><FONT COLOR="white">Vacunado: </FONT>  </label>
-           <input type="" class="" id="txtVacunado" name="txtVacunado" placeholder="Indique si ha recibido las vacunas" style="WIDTH: 240px;">
-              </center>
+           <input type="" class="" id="txtVacunado" name="txtVacunado" placeholder="Indique si ha recibido las vacunas" style="WIDTH: auto;">
+              </left>
         </div>
 
-        <div class="form-group">
-              <center>
+        <div class="form-group" style="padding: 10px">
+              <left>
            <label><FONT COLOR="white">Sexo: </FONT>  </label>
-           <input type="" class="" id="txtSexo" name="txtSexo"  placeholder="Indique si es hembra o macho" style="WIDTH: 217px;">
-              </center>
+           <input type="" class="" id="txtSexo" name="txtSexo"  placeholder="Indique si es hembra o macho" style="WIDTH: auto;">
+              </left>
         </div>
             <center><button class="btnAgregar">Agregar registro</button></center>
           </form>
       </div>
 
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-  </body>
+    </div>
 
-</html>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+@stop
