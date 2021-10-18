a0ce3d63d8db220c19de4df5792db105d72103b1
JKinvictus <91626330+JKinvictus@users.noreply.github.com>
2021-10-18 12:08:11 -0500

    tabla admin

--- WebMundoPeludo/resources/views/Admin_users.blade.php
+++ WebMundoPeludo/resources/views/Admin_users.blade.php
@@ -3,7 +3,38 @@
 @section('contenido')
 
 <div class="container-fluid" style="padding: 20px">
-    <p>Aqui va todo el contenido de la administración de usuarios</p>
+
+    <table class="table table-success table-striped">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nombre Completo</th>
+      <th scope="col">Contraseña</th>
+      <th scope="col">Departamento</th>
+      <th scope="col">Eliminar</th>
+      <th scope="col">Actualizar</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>001</td>
+      <td>El JK</td>
+      <td>123</td>
+      <td>X</td>
+      <td>A</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>001</td>
+      <td>El José</td>
+      <td>123a</td>
+      <td>X</td>
+      <td>A</td>
+    </tr>
+    <tr>
+  </tbody>
+</table>
 </div>
 
 @stop
