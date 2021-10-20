adea17e6c1bdb9fab53b7826c56b55c0f2a4c3bc
EdAlpharo <alfarovazquezeduardo@gmail.com>
2021-10-19 20:26:32 -0500

    boton y ruta (nuevo usuario)

--- WebMundoPeludo/resources/views/Admin_users.blade.php
+++ WebMundoPeludo/resources/views/Admin_users.blade.php
@@ -1,6 +1,22 @@
 @extends('base')
 
 @section('contenido')
+    <!-- https://material.io/resources/icons/?style=baseline -->
+<link href="https://fonts.googleapis.com/css2?family=Material+Icons"
+      rel="stylesheet">
+<!-- https://material.io/resources/icons/?style=outline -->
+<link href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined"
+      rel="stylesheet">
+<!-- https://material.io/resources/icons/?style=round -->
+<link href="https://fonts.googleapis.com/css2?family=Material+Icons+Round"
+      rel="stylesheet">
+<!-- https://material.io/resources/icons/?style=sharp -->
+<link href="https://fonts.googleapis.com/css2?family=Material+Icons+Sharp"
+      rel="stylesheet">
+<!-- https://material.io/resources/icons/?style=twotone -->
+<link href="https://fonts.googleapis.com/css2?family=Material+Icons+Two+Tone"
+      rel="stylesheet">
+
 <link rel="stylesheet" href="<?php echo asset('css/Admin_user.css')?>" type="text/css">
 
 
@@ -12,8 +28,14 @@
         <input class="me-2" placeholder="Ingrese el usuario a buscar" size="25">
         <button class="btn btn-outline-success">Buscar</button>
       </form>
+
     </div>
+    <div style="padding-left: 25px">
+        <a class="btn btn-outline-success button" href="{{route('NUsuarios')}}">Nuevo Usuario</a>
+    </div>
+
 </form>
+
 
 <div class="container-fluid" style="padding: 25px">
     <table class="table table-success">
