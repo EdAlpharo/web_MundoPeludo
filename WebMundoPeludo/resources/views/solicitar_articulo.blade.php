2898ffd3f4bf0b0f85f104cd1e98d62bb1de1010
EdAlpharo <alfarovazquezeduardo@gmail.com>
2021-10-20 13:16:06 -0500

    otro ajuste

--- WebMundoPeludo/resources/views/solicitar_articulo.blade.php
+++ WebMundoPeludo/resources/views/solicitar_articulo.blade.php
@@ -2,11 +2,11 @@
 
 @section('contenido')
 
-<div class="container" style="padding: 20px">
-    <p>Solicitar Artículo</p>
+<div class="container" style="padding: 35px">
+    <h3 style="text-align: center">Solicitar Artículo</h3>
 
-    <div class="dropdown btn">
-        <a class="dropbtn button btn" href="{{route('Carritoscompras')}}">Carrito de compras</a>
+    <div class="container">
+        <a class="btn btn-primary" href="{{route('Carritoscompras')}}">Carrito de compras</a>
     </div>
 </div>
 
