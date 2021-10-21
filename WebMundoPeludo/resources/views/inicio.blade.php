be11e919f0502f5eae38ab83bdf45e2745d529e9
SamuelEspino <92130542+SamuelEspino@users.noreply.github.com>
2021-10-20 20:17:40 -0500

    carruseldeimg

--- WebMundoPeludo/resources/views/inicio.blade.php
+++ WebMundoPeludo/resources/views/inicio.blade.php
@@ -2,9 +2,39 @@
 
 @section('contenido')
 
+<br>
+<br>
+
+
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{ asset('img/carrusel1.jpg')}}" class="d-block w-100" alt="500">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('img/carrusel2.jpg')}}" class="d-block w-100" alt="500">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('img/carrusel3.jpg')}}" class="d-block w-100" alt="500">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
 <div class="container-fluid" style="padding: 20px">
     <p>Aqui va todo el contenido del inicio</p>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
 
 @stop
